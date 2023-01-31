295a446e71538206930b6279c22a7f61e1871eb0
Juan GS <66971655+Boqueronix@users.noreply.github.com>
2023-01-30 23:33:28 -0500

    JavaDoc

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,17 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-    public static Scanner sc = new Scanner(System.in);
+    public static Scanner sc = new Scanner(System.in); // Scanner for user input
+
+    /**
+     * Main method which runs the program to create a table and/or generate a random number
+     */
     public static void main(String[] args) {
         StdDraw.setScale(0, 3);
         StdDraw.enableDoubleBuffering();
         addButtons();
         int runs = 0;
+        //Detects when the user clicks on a button and runs the appropriate method (createTable or generateNumber) (runs < 50 is to prevent the program from running forever)
         while (runs < 50) {
             if (StdDraw.isMousePressed()) {
                 double x = StdDraw.mouseX();
@@ -28,6 +33,9 @@ public class Main {
             }
         }
     }
+    /**
+     * Adds the buttons to the screen after they've been cleared or the program has started
+     */
     public static void addButtons() {
         StdDraw.setPenColor(StdDraw.RED);
         StdDraw.filledRectangle(0.75, 2.5, 0.5, 0.25);
@@ -40,10 +48,13 @@ public class Main {
         StdDraw.text(2.25, 2.5, "Generate Number");
         StdDraw.show();
     }
-
+    /**
+     * Creates a table with the number of trials and divisions specified by the user
+     */
     public static void createTable() {
         Integer trials = null;
         Integer divisions = null;
+        //Gets the number of trials and divisions from the user
         while (trials == null || divisions == null) {
             System.out.println("Enter a number of trials and divisions: ");
             try {
@@ -54,10 +65,12 @@ public class Main {
                 sc.nextLine();
             }
         }
+        //Creates an ArrayList of random numbers
         ArrayList<Double> results = new ArrayList<>(trials);
         for (int i = 0; i < trials; i++) {
             results.add(Math.random());
         }
+        //Creates a HashMap with the number of times each division occurs within results
         HashMap<Integer, Double> map = new HashMap<>();
         for (int i = 0; i < divisions; i++) {
             int count = 0;
@@ -69,6 +82,7 @@ public class Main {
             map.put(i, count * 1.0 / results.size());
         }
         StdDraw.clear();
+        //Draws the table
         StdDraw.square(1.5, 1.5, 0.5);
         for (int i = 0; i < 11; i++) {
             StdDraw.text(0.9, 1 + i * 0.1, i * 10 + "%");
@@ -76,6 +90,7 @@ public class Main {
                 StdDraw.text(1 + i * 0.1, 0.9, i * 10 + "%");
             }
         }
+        //Draws the bars in the table with the appropriate data
         for (int i = 0; i < divisions; i++) {
             StdDraw.setPenColor(StdDraw.BLUE);
             StdDraw.filledRectangle(1 + (i + 0.5) / divisions, map.get(i) / 2.0 + 1, 0.5 / divisions, map.get(i) / 2.0);
@@ -83,10 +98,13 @@ public class Main {
             StdDraw.rectangle(1 + (i + 0.5) / divisions, map.get(i) / 2.0 + 1, 0.5 / divisions, map.get(i) / 2.0);
         }
     }
-
+    /**
+     * Generates a random number between the min and max specified by the user
+     */
     public static void generateNumber() {
         Integer min = null;
         Integer max = null;
+        //Gets the min and max from the user
         while (min == null || max == null) {
             System.out.println("Enter min and max: ");
             try {
@@ -97,6 +115,7 @@ public class Main {
                 sc.nextLine();
             }
         }
+        //Generates a random number between min and max and displays it
         StdDraw.clear();
         StdDraw.setPenColor(StdDraw.BLACK);
         int range = max - min + 1;
